3dc6e7b888baaaab51f2d7fd7c76696604ccfd5f
Erin Deuel and Molly Stambaugh <erin@>
2019-10-09 16:18:02 -0700

    add place index and show route

--- Places/Controllers/PlacesController.cs
+++ Places/Controllers/PlacesController.cs
@@ -23,6 +23,11 @@ namespace Places.Controllers
             Place myPlace = new Place(cityName);
             return RedirectToAction("Index");
         }
-
+        [HttpGet("/places/{id}")]
+        public ActionResult Show (int id)
+        {
+            Place foundPlace = Place.Find(id);
+            return View(foundPlace);
+        }
     }
 }
--- Places/Models/Places.cs
+++ Places/Models/Places.cs
@@ -5,17 +5,23 @@ namespace Places.Models
     public class Place
     {
         public string CityName { get; set; }
+        public int Id { get; }
         private static List<Place> _instances = new List<Place> {};
 
         public Place (string cityName)
         {
             CityName = cityName;
             _instances.Add(this);
+            Id = _instances.Count;
 
         }
         public static List<Place> GetAll()
         {
             return _instances;
+        }
+        public static Place Find(int searchId)
+        {
+            return _instances[searchId-1];
         }
     }
 }
